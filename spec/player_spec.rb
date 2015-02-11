d529c759136a0539329f0878328623aba1a32718
tommasobratto <tbratto22@gmail.com>
2015-02-11 11:15:11 +0000

    added ship_spec file

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,5 @@
 require 'player'
 
 describe Player do
-
-  it 'should be able to make a move' do 
-    expect(player.make_move).to eq true
-  end
   
 end
---
+++ spec/ship_spec.rb
@@ -0,0 +1,9 @@
+require 'ship'
+
+describe Ship do
+
+  it "should have a size" do
+    expect(ship.size).to eq 1
+  end
+  
+end
