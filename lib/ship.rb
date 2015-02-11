432c0e28b7dde848f82f018e25706cc6267b91e8
tommasobratto <tbratto22@gmail.com>
2015-02-11 12:40:21 +0000

    added sunk? method, all tests pass

--- lib/ship.rb
+++ lib/ship.rb
@@ -12,8 +12,28 @@ class Ship
     new(2, 'destroyer')
   end
 
+  def self.cruiser
+    new(3, 'cruiser')
+  end
+
+  def self.submarine
+    new(3, 'submarine')
+  end
+
+  def self.battleship
+    new(4, 'battleship')
+  end
+
+  def self.carrier
+    new(5, 'carrier')
+  end
+
   def hit
     @hit_points = hit_points - 1
+  end
+
+  def sunk?
+    @hit_points == 0
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,25 +2,24 @@ require 'ship'
 
 describe Ship do
 
+  let(:ship) { Ship.destroyer }
+
   it "should know what's going to be its name" do
-    ship = Ship.destroyer
     expect(ship.name).to eq 'destroyer'
   end
 
   it "should know the size of a destroyer" do
-    ship = Ship.destroyer
     expect(ship.size).to eq 2
   end
 
   it "should know when it has been hit" do
-    ship = Ship.destroyer
     ship.hit
     expect(ship.hit_points).to eq 1
   end
 
-  it 'should know when it has been sunk' do
-    ship.hit
-    expect(ship.sunk).to eq true
+  it "should know when it has been sunk" do
+    2.times { ship.hit }
+    expect(ship).to be_sunk
   end
 
 end
